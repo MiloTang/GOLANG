9403bba58e6dd67291cda3a3f43d352479f4369a
miloit <miloit@qq.com>
2017-03-27 15:00:22 +0800

    update20170327005

--- golib/main.go
+++ golib/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golib/godb/gomysql"
+	"golib/gofile"
 )
 
 func init() {
@@ -11,4 +12,5 @@ func main() {
 	gomysql.Connect()
 	defer gomysql.Close()
 	fmt.Println(gomysql.Select("select * from guests"))
+	fmt.Println(gofile.ListDirs("c:/Milo/boxs/"))
 }
